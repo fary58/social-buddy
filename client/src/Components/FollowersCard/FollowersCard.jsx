cf4d693be875cce2368444fcb2e4cefcf4eb7500
Fary~ <65035293+fary58@users.noreply.github.com>
2025-04-10 03:01:58 +0100

    user info screen added

--- client/src/Components/FollowersCard/FollowersCard.jsx
+++ client/src/Components/FollowersCard/FollowersCard.jsx
@@ -1,26 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import UserImg from "../../img/img1.png";
-import './FollowersCard.css'
-
+import "./FollowersCard.css";
 
 const FollowersCard = () => {
   return (
     <div className="FollowersCard">
- <h3>People you may follow</h3>
-<div className="follower">
-    <div>
-        <img src={UserImg} alt="" className='followerImage' />
-                            <div className="name">
-                                <span>John</span>
-                                <span>@Johndoe</span>
-                            </div>
-                        </div>
-                        <button className='button fc-button' >
-Unfollow                        </button>
-
+      <h3>People you may follow</h3>
+      <div className="follower">
+        <div>
+          <img src={UserImg} alt="" className="followerImage" />
+          <div className="name">
+            <span>John</span>
+            <span>@Johndoe</span>
+          </div>
+        </div>
+        <button className="button fc-button">Unfollow </button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default FollowersCard
+export default FollowersCard;
